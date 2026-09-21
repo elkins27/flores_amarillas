7a0622872c422313bc0e651fa9c211184394a32b
elkins27 <elkin.sanqui05@gmail.com>
2026-09-20 22:00:34 -0500

    Update messages.js

--- js/messages.js
+++ js/messages.js
@@ -1,6 +1,6 @@
 export const mensajes = [
   "Hola mi amor… 👀💛",
-  "Para ti miña, porque haces que mis días florezcan. 🌻💛",
+  "Para ti niña, porque haces que mis días florezcan. 🌻💛",
   "No son solo flores, es un pequeño pedacito de mi amor para ti. 💛",
   "Un pequeño detalle, unas flores amarillas que aunq sea virtuales fueron hechas con cariño. 🌻💛",
   "Tú haces que hasta los días grises tengan un poquito de color. 🌻",
