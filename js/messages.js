f2a81ab6fcb362cc169622b9c71281892a7b1f1e
elkins27 <elkin.sanqui05@gmail.com>
2026-09-20 21:08:21 -0500

    Update messages.js

--- js/messages.js
+++ js/messages.js
@@ -1,8 +1,8 @@
 export const mensajes = [
   "Hola mi amor… 👀💛",
   "Para ti miña, porque haces que mis días florezcan. 🌻💛",
-  "No son solo flores, es un pequeño pedacito de mi cariño para ti. 💛",
-  "Un pequeño detalle, unas flores amarillas que reflejen un poquito de todo el amor que te tengoo. 🌻💛",
+  "No son solo flores, es un pequeño pedacito de mi amor para ti. 💛",
+  "Un pequeño detalle, unas flores amarillas que aunq sea virtuales fueron hechas con cariño. 🌻💛",
   "Tú haces que hasta los días grises tengan un poquito de color. 🌻",
   "Que estas flores te recuerden que hay alguien que siempre piensa en ti, que se siente afortunadoo de tenerte en su vida y que siempre quiere verte sonreír. 💛🌻",
   "Feliz Día de las Flores Amarillas, mi Juleisitaa. TE AMOOO 💛🌻",
